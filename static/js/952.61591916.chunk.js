018030a2f56ba8802da6d7ddc378af8803b5da08
zurabT <157989526+zurabT@users.noreply.github.com>
2025-06-10 23:05:30 +0400

    Updates

---
+++ static/js/952.61591916.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkflight=self.webpackChunkflight||[]).push([[952],{4952:e=>{e.exports=function(){throw new Error("ws does not work in the browser. Browser clients must use the native WebSocket object")}}}]);
+//# sourceMappingURL=952.61591916.chunk.js.map
